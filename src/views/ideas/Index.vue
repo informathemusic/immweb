873835e36e5f1b1cfe7e9ba4289beccc6620a7a6
informathemusic <informathemusic@DESKTOP-1N5151BL3-N0TL4PT0P.lan>
2020-06-21 22:43:45 +0200

    Fixed some bugs in ideas

--- src/views/ideas/Index.vue
+++ src/views/ideas/Index.vue
@@ -1,8 +1,8 @@
 <template>
     <v-app>
-        <h1>I am {{open?'':'not'}} available for code ideas!</h1>
-        <h2 v-if="!open">This happens when i'm already doing a project or not in a mood to code (Happens)</h2>
-        <a href="mailto:informathemusic@gmail.com?Subject=Idea">E-mail me an idea{{open?'':' anyways'}}!</a>
+        <h1>I am {{!open?'':'not'}} available for code ideas!</h1>
+        <h2 v-if="!!open">This happens when i'm already doing a project or not in a mood to code (Happens)</h2>
+        <a href="mailto:informathemusic@gmail.com?Subject=Idea">E-mail me an idea{{!open?'':' anyways'}}!</a>
     </v-app>
 </template>
 
