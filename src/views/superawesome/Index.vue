16fc0f646b4ddd57e9640b0ca1a5ece3c3ed4910
informathemusic <informathemusic@DESKTOP-1N5151BL3-N0TL4PT0P.lan>
2020-07-15 17:49:15 +0200

    Added Superawesome

--- src/views/superawesome/Index.vue
+++ src/views/superawesome/Index.vue
@@ -1,11 +1,14 @@
 <template>
-    <h1>Please wait, page is loading...<h1>
+<v-app>
+    <h1>DMCa compliant rick roll</h1>
+    <p>Never gonna give you up never gonna let you down never gonna run around and desert you never gonnamake you cry never gonna say goodbye never gonna tell a lie and hurt you</p>
+    </v-app>
 </template>
 
 <script>
 export default {
-    mounted() {
+    /*mounted() {
         setTimeout(()=>location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ",2000)
-    }
+    }*/
 }
 </script>
