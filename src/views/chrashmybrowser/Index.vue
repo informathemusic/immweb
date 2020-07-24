e39afa337e64fc3c19ba3c883fc51f3d83dc643d
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2020-07-24 19:07:40 +0200

    Update Index.vue

--- src/views/chrashmybrowser/Index.vue
+++ src/views/chrashmybrowser/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <h1>This is a (probably failed) attempt to chrash your browser.</h1>
+    <h2>Please avoid if you don't have much ram and/or are on firefox, and make sure nothing is running in the background.<br/>I decline any responsibility if this page actually breaks anything.</h2>
     <v-btn @click="chrash">(probably don't) chrash my browser</v-btn>
   </v-app>
 </template>
