d27989a378540dbe75191a8afc1096ca2f499bd1
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2021-01-15 22:50:32 +0100

    Create Index.vue

---
+++ src/views/SmallProjects/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <p class="display-1 text--primary">
+        All of my small projects
+      </p>
+      <div class="text--primary">
+        I'm looking for...
+        <v-autocomplete :items="Object.keys(urls)"></v-autocomplete>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Open in new tab
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    urls: {
+      'Meme License': 'https://42.imm.codes/meme-license',
+      'CDN': 'https://cdn.imm.codes/',
+      'Safe for Advent of Code solutions': 'https://aocsafe.imm.codes/',
+      'Proxy': 'https://bypass.imm.codes/bypass-imm',
+      'Cute Domain Parking': 'https://cdp.imm.codes/',
+      'Last Barrier': 'https://lb.imm.codes/',
+      'Pain Au Chocolat, pas Chocolatine': 'https://pac.imm.codes/invite.html',
+      'Backup Bot': 'https://backupbot.imm.codes/invite.html',
+      'Reaction Bot': 'https://discord.com/api/oauth2/authorize?client_id=744834906986643554&permissions=268435456&scope=bot'
+    }
+  }),
+  created() {
+    this.chars=this.chars.map((v)=>{
+      v.list.sort((a,b)=>Object.entries(this.ranking).find(([k])=>k==a.spottable)[1].rank-Object.entries(this.ranking).find(([k])=>k==b.spottable)[1].rank)
+      return v
+    })
+  }
+};
+</script>
+
+<style>
+.card{
+  padding:10 !important;
+}
+</style>
